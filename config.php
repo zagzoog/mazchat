809f66f5c52d15ea20db70919aa92f41cb0a3a45
Mohammed Zagzoog <zagzoog@alanaml.com>
2025-03-26 16:33:53 +0300

    Updated ssl for server

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/environment.php';
 $config = [
     'test' => [
         'webhook_url' => 'https://n8n.mazcode.com/webhook/b0e277cb-a3ab-40be-9c7d-2048c6bbad8f',
-        'ssl_verify' => false,
+        'ssl_verify' => true,
         'conversations_per_page' => 10,
         'debug' => true,
         'development_mode' => true,
@@ -21,15 +21,17 @@ $config = [
         'gold_price' => 30,
         'paypal_client_id' => 'AS2_uTbJJFC56yZqOv6Y_CfFZNLNCKkV_z13UW8sjcWhkq3dU93o8H1hhr7gKOnujp1NfNB5oj3xe2bf',
         'paypal_secret' => 'EDGDx3mheykckofj14L7Zud6CUPU0kap4lwZLSzm41D51AmWBacoPUf9-W-MI6_9e75uVDlBvAvRXSIe',
-        'paypal_mode' => 'sandbox'
+        'paypal_mode' => 'sandbox',
+        'trusted_proxies' => ['cloudflare'],
+        'force_ssl' => true
     ],
     'production' => [
         'webhook_url' => 'https://n8n.mazcode.com/webhook/b0e277cb-a3ab-40be-9c7d-2048c6bbad8f',
-        'ssl_verify' => false,
-        'debug' => true,
+        'ssl_verify' => true,
+        'debug' => false,
         'conversations_per_page' => 10,
-        'development_mode' => true,
-        'debug_logging' => true,
+        'development_mode' => false,
+        'debug_logging' => false,
         'free_monthly_limit' => 200,
         'silver_monthly_limit' => 100,
         'gold_monthly_limit' => 999999,
@@ -40,7 +42,9 @@ $config = [
         'gold_price' => 30,
         'paypal_client_id' => 'AS2_uTbJJFC56yZqOv6Y_CfFZNLNCKkV_z13UW8sjcWhkq3dU93o8H1hhr7gKOnujp1NfNB5oj3xe2bf',
         'paypal_secret' => 'EDGDx3mheykckofj14L7Zud6CUPU0kap4lwZLSzm41D51AmWBacoPUf9-W-MI6_9e75uVDlBvAvRXSIe',
-        'paypal_mode' => 'sandbox'
+        'paypal_mode' => 'sandbox',
+        'trusted_proxies' => ['cloudflare'],
+        'force_ssl' => true
     ]
 ];
 
