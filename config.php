eaa9df751a3925947e3a4085c2004ec4c23f9a72
Mohammed Zagzoog <zagzoog@alanaml.com>
2025-03-26 18:36:05 +0300

    Updated ssl for production

--- config.php
+++ config.php
@@ -25,8 +25,8 @@ $config = [
     ],
     'production' => [
         'webhook_url' => 'https://n8n.mazcode.com/webhook/b0e277cb-a3ab-40be-9c7d-2048c6bbad8f',
-        'ssl_verify' => false,
-        'debug' => true,
+        'ssl_verify' => true,
+        'debug' => false,
         'conversations_per_page' => 10,
         'development_mode' => true,
         'debug_logging' => true,
